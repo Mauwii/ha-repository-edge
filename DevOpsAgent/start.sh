44507be70410185022f9aa0fbd9b8f9b0cf888da
Mauwii <mauwii@outlook.de>
2022-08-04 22:14:24 +0200

    get env from config with bashio

--- DevOpsAgent/start.sh
+++ DevOpsAgent/start.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
+AZP_AGENT_NAME=$(bashio::config 'agentname')
+AZP_POOL=$(bashio::config 'pool')
+AZP_URL=$(bashio::config 'organization')
+AZP_TOKEN=$(bashio::config 'token')
+
 if [ -z "$AZP_URL" ]; then
   echo 1>&2 "error: missing AZP_URL environment variable"
   exit 1
